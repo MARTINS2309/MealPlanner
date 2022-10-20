aa0cdfdccdfbb838465cadd30bb9562ab8e7ffe6
MARTINS2309 <martins2309@gmail.com>
2022-10-20 13:03:39 +0100

    express routes

--- express/src/routes/ingredients.js
+++ express/src/routes/ingredients.js
@@ -3,27 +3,38 @@ const router = express.Router();
 const request = require("request");
 
 router.get("/", (req, res) => {
-  var query = req.query.query ?? "100g egg";
-
-  request.get(
-    {
-      url: "https://api.calorieninjas.com/v1/nutrition?query=" + query,
-      headers: {
-        "X-Api-Key": process.env.API_key_CalorieNinjas,
-      },
-    },
-    function (error, response, body) {
-      if (error) return console.error("Request failed:", error);
-      else if (response.statusCode != 200)
-        return console.error(
-          "Error:",
-          response.statusCode,
-          body.toString("utf8")
-        );
-      else res.send(body);
-    }
-  );
   console.log("GET ingredients");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    request.get(
+      {
+        url:
+          "https://api.calorieninjas.com/v1/nutrition?query=" + req.params.id,
+        headers: {
+          "X-Api-Key": process.env.API_key_CalorieNinjas,
+        },
+      },
+      function (error, response, body) {
+        if (error) return console.error("Request failed:", error);
+        else if (response.statusCode != 200)
+          return console.error(
+            "Error:",
+            response.statusCode,
+            body.toString("utf8")
+          );
+        else res.send(body);
+      }
+    );
+  })
+  .put((req, res) => {})
+  .delete((req, res) => {});
+
+router.param("id", (req, res, next, id) => {
+  console.log(`ingredient id:${id}`);
+  next();
 });
 
 module.exports = router;
